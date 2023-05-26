0b95e0bfe7063a37caeee87a5837f91f52978f1a
Dominik Engelen <119937576+LoLFuchs@users.noreply.github.com>
2023-05-26 17:23:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 isTrue = []
 isFalse = []
 
-with open("C:\Users\domin\OneDrive\Desktop\wichtig\Programieren\VCS\Dataag\wert.txt", "r") as file:
-    wert = str(file.read())
+wert = open("wert.txt","r")
 
 # Hier kannst du den gelesenen Wert in Datei B verwenden
-print("Der gelesene Wert lautet:", str(wert))
+print(wert.read())
